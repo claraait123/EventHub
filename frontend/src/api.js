c5bbcea6075d2c9fb35602e031943d087a1c6d92
Clara AM <rabiaautop@gmail.com>
2026-04-01 11:54:41 +0200

    resolved api issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-fetch(`${apiUrl}/api/events`)
+const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
-// Ajoute le token à chaque requête automatiquement
+const api = axios.create({
+  baseURL: apiUrl,
+});
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
